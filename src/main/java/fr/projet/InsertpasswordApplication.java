374f36aca0425aac2ff4aeb440e3c09811c7a583
mmhh2024 <hayett.makhlouf@gmail.com>
2024-06-13 22:48:16 +0200

    Version V2-Valide

--- src/main/java/fr/projet/InsertpasswordApplication.java
+++ src/main/java/fr/projet/InsertpasswordApplication.java
@@ -1,13 +1,94 @@
 package fr.projet;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.SpringBootApplication; 
 
 @SpringBootApplication
 public class InsertpasswordApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InsertpasswordApplication.class, args);
-	}
 
+	  final Logger logger = LoggerFactory.getLogger(InsertpasswordApplication.class);
+      /* @Value("${path.in}")
+	  String repin;
+	  @Value("${path.out}")
+	  String repout ;
+	  final  File repertoirein = new File(repin); 
+	  final  File repertoireout = new File(repout); 
+	  */
+	  final  File repertoirein = new File("C:\\DATAPASSWORD\\IN\\"); 
+	  final  File repertoireout = new File("C:\\DATAPASSWORD\\TRAITE\\"); 
+         
+    	if (repertoirein.isDirectory()) {
+			System.out.println("répertoire");
+			int batchIndex = 0;
+            
+			File[] listOfFiles = repertoirein.listFiles();
+			if(listOfFiles.length > 0) {
+
+				try (Connection connection = 
+      			DriverManager.getConnection("jdbc:clickhouse://127.0.0.1:8123/passworddb", 
+				"default", "")) {
+					connection.setAutoCommit(false);
+			
+					for (int i = 0; i < listOfFiles.length; i++){
+						batchIndex = 0;
+						logger.info("Traitement du fichier"+listOfFiles[i].getName());
+						
+						String fileName = repertoirein + "\\" + listOfFiles[i].getName();
+						
+						String buffer ;
+						String[] bufferTab;
+					
+						File file = new File(fileName);
+						fileName = repertoireout + "\\" + listOfFiles[i].getName()+".ok";
+						
+						File fileout = new File(fileName);
+						
+						Scanner scanner = new Scanner(file);
+						
+						try (PreparedStatement statement = 
+								connection.prepareStatement("INSERT INTO passwordtbl  VALUES (?)")) {
+							while (scanner.hasNextLine()){
+								buffer = scanner.nextLine();
+								batchIndex++;
+								
+								bufferTab = buffer.split(";");
+								if (bufferTab.length > 0){
+										statement.setString(1, bufferTab[0]);	
+										
+										statement.addBatch();
+
+										if (batchIndex == 100_000) {
+											statement.executeBatch();
+											connection.commit();
+											batchIndex = -1;
+										}									
+								}					 
+							} 
+							statement.executeBatch();
+							connection.commit();
+							scanner.close();	
+							file.renameTo(fileout);
+							
+						}	
+            		}
+				  	  
+				} catch (Exception ex) {
+					ex.printStackTrace();
+					logger.error("Impossible de se connecter ...");
+				}
+			} else {
+				logger.info("Répertoire vide ...");
+			}
+		} 
+		
+}
 }
